34c817448d63b820b93e9f6b1424859ed745b357
Imad Toubal <imad.toubal@gmail.com>
2022-08-30 20:33:31 -0500

    Use consistent spaces

--- setup.py
+++ setup.py
@@ -5,19 +5,18 @@ from setuptools import Extension, setup
 
 # Obtain the numpy include directory.  This logic works across numpy versions.
 try:
-    numpy_include = numpy.get_include()
+  numpy_include = numpy.get_include()
 except AttributeError:
-    numpy_include = numpy.get_numpy_include()
+  numpy_include = numpy.get_numpy_include()
 
 example_module = Extension('_example',
                            sources=['example_wrap.cxx', 'src/example.cpp'],
-                           include_dirs=['src', numpy_include],
-                           )
+                           include_dirs=['src', numpy_include])
 
-setup (name = 'example',
-       version = '0.1',
-       author      = "SWIG Docs",
-       description = """Simple swig example from docs""",
-       ext_modules = [example_module],
-       py_modules = ["example"],
-       )
+setup(name='example',
+      version='0.1',
+      author="SWIG Docs",
+      description="""Simple swig example from docs""",
+      ext_modules=[example_module],
+      py_modules=["example"],
+      install_requires=['numpy'])
--- src/example.cpp
+++ src/example.cpp
@@ -1,27 +1,27 @@
-#include <stdio.h>
 #include <math.h>
+#include <stdio.h>
 
 #include "example.h"
 
 double rms(double *seq, int n) {
-    double mean = 0.0;
-    for (int i = 0; i < n; i++) {
-        mean += seq[i];
-    }
-    mean /= n;
-    double rms = 0.0;
-    for (int i = 0; i < n; i++) {
-        rms += (seq[i] - mean) * (seq[i] - mean);
-    }
-    rms /= n;
-    return sqrt(rms);
+  double mean = 0.0;
+  for (int i = 0; i < n; i++) {
+    mean += seq[i];
+  }
+  mean /= n;
+  double rms = 0.0;
+  for (int i = 0; i < n; i++) {
+    rms += (seq[i] - mean) * (seq[i] - mean);
+  }
+  rms /= n;
+  return sqrt(rms);
 }
 
-void binarize(double* seq, int n, int m) {
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            int index = i * m + j;
-            seq[index] = seq[index] > 127 ? 255 : 0;
-        }
+void binarize(double *seq, int n, int m) {
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < m; j++) {
+      int index = i * m + j;
+      seq[index] = seq[index] > 127 ? 255 : 0;
     }
+  }
 }
